22e8f6cda19836e5ff5ce6b05c8686b5715658a6
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-10 22:45:20 +0700

    Creating the popular page

---
+++ src/component/Popular.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+
+const Popular = () => {
+	useEffect(() => {
+		getPopular();
+	}, []);
+
+	const getPopular = async () => {
+		const api = await fetch(
+			`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_API_KEY}&number=9`
+		);
+		const data = await api.json();
+		console.log(data);
+	};
+
+	return <div>Popular</div>;
+};
+
+export default Popular;
