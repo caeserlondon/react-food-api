9d8984b0d77e976efad688c2bf97e3f3697bba96
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-10 22:40:14 +0700

    Creating the home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Popular from '../component/Popular';
+import Veggie from '../component/Veggie';
+
+import React from 'react';
+
+function Home() {
+	return (
+		<div>
+			<Popular />
+			<Veggie />
+		</div>
+	);
+}
+
+export default Home;
