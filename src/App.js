30014d57a9d0a14a710ecc4b86cf027b901f2a17
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-11 18:39:06 +0700

    Adding the Category Component to the App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Pages from './pages/Pages';
+import Category from './component/Category';
 
 function App() {
 	return (
 		<div className="App">
 			<h1>yum yum</h1>
+			<Category />
 			<Pages />
 		</div>
 	);
