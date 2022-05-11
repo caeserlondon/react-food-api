541396280ccb1e2b66cc234b908050ca174a3f05
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-11 23:25:10 +0700

    Updating the Search component

--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import styled from 'styled-components';
+import { useState } from 'react';
+import { FaSearch } from 'react-icons/fa';
 
 function Search() {
-	return <div>Search</div>;
+	return (
+		<form>
+			<input type="text" />
+		</form>
+	);
 }
 
 export default Search;
