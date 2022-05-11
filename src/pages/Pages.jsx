587eba369979956d5abc3269e79dcdbfbd1a6df5
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-11 23:17:37 +0700

    Updating & adding the routes

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import Home from './Home';
+import Cuisine from './Cuisine';
+import { Routes, Route } from 'react-router-dom';
 
 function Pages() {
-	return <Home />;
+	return (
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/cuisine/:type" element={<Cuisine />} />
+		</Routes>
+	);
 }
 
 export default Pages;
