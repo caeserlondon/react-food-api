09677d122b6c4a65142ad8afe9fa816e62f0c0bb
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-12 01:43:40 +0700

    Adding the navigate functunality to the Search component

--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -1,12 +1,15 @@
 import styled from 'styled-components';
 import { FaSearch } from 'react-icons/fa';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Search() {
 	const [input, setInput] = useState('');
+	const navigate = useNavigate();
 
 	const submitHandler = (e) => {
 		e.preventDefault();
+		navigate('/searched/' + input);
 	};
 
 	return (
