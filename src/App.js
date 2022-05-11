50c5588648fc154d24ffca3c1e9e7ec4c2c3b849
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-11 23:42:20 +0700

    Adding the Search component to the App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Pages from './pages/Pages';
 import Category from './component/Category';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Search from './component/Search';
+
 function App() {
 	return (
 		<div className="App">
 			<Router>
+				<Search />
 				<Category />
 				<Pages />
 			</Router>
