a17698e69ae8352d281911eb68018a392729b7c0
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-12 15:36:56 +0700

    Updating the Logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import Pages from './pages/Pages';
 import Category from './component/Category';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import Search from './component/Search';
+import styled from 'styled-components';
+import { GiForkKnifeSpoon } from 'react-icons/gi';
 
 function App() {
 	return (
 		<div className="App">
 			<Router>
+				<Nav>
+					<Logo to={'/'}> Yum Yum</Logo>
+					<GiForkKnifeSpoon />
+				</Nav>
 				<Search />
 				<Category />
 				<Pages />
@@ -14,5 +20,25 @@ function App() {
 		</div>
 	);
 }
+
+const Logo = styled(Link)`
+	text-decoration: none;
+	font-size: 2rem;
+	font-weight: 600;
+	color: red;
+	font-family: 'Water Brush', cursive;
+`;
+
+const Nav = styled.div`
+	padding: 4rem 0rem;
+	display: flex;
+	justify-content: flex-start;
+	align-items: center;
+	svg {
+		font-size: 2rem;
+		color: orange;
+		margin-left: 1rem;
+	}
+`;
 
 export default App;
